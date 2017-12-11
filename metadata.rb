3532451e57b3eb2ea20d6f775c2fac059eb1d971
Nick Gray <f0rkz@f0rkznet.net>
2017-12-10 22:09:23 -0500

    First commit

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,14 @@
 name 'plexpy'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures plexpy'
-long_description 'Installs/Configures plexpy'
-version '0.1.0'
+maintainer 'Nick Gray'
+maintainer_email 'f0rkz@f0rkznet.net'
+license 'Apache-2.0'
+description 'Installs plexpy https://github.com/JonnyWong16/plexpy'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.2.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+issues_url 'https://github.com/f0rkz/chef-plexpy/issues'
+source_url 'https://github.com/f0rkz/chef-plexpy'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/plexpy/issues'
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/plexpy'
+%w(ubuntu debian).each do |os|
+  supports os
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,83 @@
 # Cookbook:: plexpy
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Author:: Nick Gray (f0rkz@f0rkznet.net)
+#
+# Copyright:: 2017 f0rkznet.net
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+package 'git' do
+  action :install
+end
+
+package 'virtualenv' do
+  action :install
+end
+
+user node['plexpy']['deploy_user'] do
+  comment 'Plexpy deployment user'
+  shell '/bin/sh'
+  system true
+end
+
+directory node['plexpy']['deploy_dir'] do
+  owner node['plexpy']['deploy_user']
+  group node['plexpy']['deploy_user']
+  mode '0755'
+  recursive true
+  action :create
+end
+
+execute 'install virtualenv' do
+  command <<-EOF
+  virtualenv #{node['plexpy']['deploy_dir']}/virtualenv
+  EOF
+  user node['plexpy']['deploy_user']
+  group node['plexpy']['deploy_user']
+  action :run
+  not_if { ::File.directory?("#{node['plexpy']['deploy_dir']}/virtualenv") }
+end
+
+directory "#{node['plexpy']['deploy_dir']}/plexpy" do
+  owner node['plexpy']['deploy_user']
+  group node['plexpy']['deploy_user']
+  mode '0755'
+  recursive true
+  action :create
+end
+
+git "#{node['plexpy']['deploy_dir']}/plexpy" do
+  repository 'https://github.com/JonnyWong16/plexpy.git'
+  user node['plexpy']['deploy_user']
+  group node['plexpy']['deploy_user']
+  action :sync
+end
+
+template '/etc/systemd/system/plexpy.service' do
+  source 'plexpy.service.erb'
+  owner node['plexpy']['deploy_user']
+  group node['plexpy']['deploy_user']
+  mode '0744'
+  variables({
+    deploy_user: node['plexpy']['deploy_user'],
+    deploy_dir: node['plexpy']['deploy_dir'],
+    venv_python: "#{node['plexpy']['deploy_dir']}/virtualenv/bin/python",
+    plexpy_port: node['plexpy']['port']
+  })
+end
+
+service 'plexpy' do
+  supports status: true
+  action [:enable, :start]
+end
